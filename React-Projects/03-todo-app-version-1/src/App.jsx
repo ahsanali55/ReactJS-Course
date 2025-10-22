78a29c71cf36a2b66d2e7f658ba19191dc79d5d9
ahsanali555 <ahsanalijawad599@gmail.com>
2025-10-22 11:05:16 +0500

    Yesterday TodoApp bugs solved.

--- React-Projects/03-todo-app-version-1/src/App.jsx
+++ React-Projects/03-todo-app-version-1/src/App.jsx
@@ -1,64 +1,23 @@
-import { useState, useReducer } from "react";
 import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
 import TodoItems from "./components/TodoItems";
 import style from "./App.module.css";
-import WelcomeMessage from "./components/WelcomeMessage";
-import { TodoItemsContext } from "./store/todo-items-store";
-// import todoItemContextProvider from './store/todo-items-store';
-
-//Reducer fuction
-let todoItemReducer = (currTodoItems, action) => {
-
-  let newTodoItems = currTodoItems;
-  if (action.type === "NEW_ITEM"){
-    newTodoItems = [
-      ...currTodoItems, {name: action.payload.itemName, 
-        date: action.payload.itemDueDate }
-    ]
-  }
-  else if (action.type === "DELETE_ITEM"){
-    newTodoItems = currTodoItems.filter(item => {
-      return (item.name !== action.payload.todoName)
-    });
-  }
-  return newTodoItems;
-}
+import WelcomeMessage from "./components/WelcomeMessage";   
+import TodoItemContextProvider from "./store/todo-items-store";
 
 function App() {
-   ///  currState   // dispatch                     Reducer,        initialState
-  const [todoItems, dispatchTodoItems] = useReducer(todoItemReducer, [])
-
-  // Add new item in Todo Items Method
-  const addNewItem = (itemName, itemDueDate) => {
-      
-    const newItemAction ={
-      type: "NEW_ITEM",
-      payload:{
-         itemName,
-        itemDueDate,
-      }
-    };
-    // dispatch Action
-  dispatchTodoItems(newItemAction)
-  }
+   
   
   return (
     <>
-      <TodoItemsContext.Provider value={{
-        // key values are same
-        todoItems,  // Array
-        addNewItem, // Methods
-        dispatchTodoItems
-      }}>
-
+      <TodoItemContextProvider>
       <center className={`${style["todo-container"]}`}>
         <AppName />
         <AddTodo />
         <WelcomeMessage />
         <TodoItems />
       </center>
-      </TodoItemsContext.Provider>
+      </TodoItemContextProvider>
 
     </>
   );
--- React-Projects/03-todo-app-version-1/src/store/todo-items-store.jsx
+++ React-Projects/03-todo-app-version-1/src/store/todo-items-store.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { Children, createContext, useReducer } from "react";
 
 export const TodoItemsContext = createContext([{
         // key values are same
@@ -7,25 +7,54 @@ export const TodoItemsContext = createContext([{
         deleteItem: () => {},
       }]);
 
-// const todoItemContextProvider = () => {
-//     ///  currState   // dispatch                     Reducer,        initialState
-//   const [todoItems, dispatchTodoItems] = useReducer(todoItemReducer, [])
+//Reducer fuction
+let todoItemReducer = (currTodoItems, action) => {
 
-//   // Add new item in Todo Items Method
-//   const addNewItem = (itemName, itemDueDate) => {
+  let newTodoItems = currTodoItems;
+  if (action.type === "NEW_ITEM"){
+    newTodoItems = [
+      ...currTodoItems, {name: action.payload.itemName, 
+        date: action.payload.itemDueDate }
+    ]
+  }
+  else if (action.type === "DELETE_ITEM"){
+    newTodoItems = currTodoItems.filter(item => {
+      return (item.name !== action.payload.todoName)
+    });
+  }
+  return newTodoItems;
+}
+
+const TodoItemContextProvider = (props) => {
+///  currState   // dispatch                     Reducer,        initialState
+  const [todoItems, dispatchTodoItems] = useReducer(todoItemReducer, [])
+
+  // Add new item in Todo Items Method
+  const addNewItem = (itemName, itemDueDate) => {
       
-//     const newItemAction ={
-//       type: "NEW_ITEM",
-//       payload:{
-//          itemName,
-//         itemDueDate,
-//       }
-//     };
-//     // dispatch Action
-//   dispatchTodoItems(newItemAction)
-//   };
-// }
-// default export todoItemContextProvider;
+    const newItemAction = {
+      type: "NEW_ITEM",
+      payload:{
+         itemName,
+        itemDueDate,
+      }
+    };
+    // dispatch Action
+  dispatchTodoItems(newItemAction);
+  }
+  return(
+    <>
+     <TodoItemsContext.Provider 
+     value={{
+        // key values are same
+        todoItems,  // Array
+        addNewItem, // Methods
+        dispatchTodoItems
+      }}>
+        {props.children}
+      </TodoItemsContext.Provider>
+    </>
+  )
+  }
 
-
-
+export default TodoItemContextProvider;
